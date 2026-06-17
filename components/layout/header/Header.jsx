789247c9f916fe59dab25debfe7c43a571c1aef4
Ahmad <ahmad.pakmehr@gmail.com>
2026-06-17 23:44:19 +0200

    Add logo placeholder

--- components/layout/header/Header.jsx
+++ components/layout/header/Header.jsx
@@ -4,6 +4,7 @@ import styles from "./Header.module.css";
 export default function Header() {
 	return (
 		<header className={styles.header}>
+			<div className={styles.logoPlaceholder}>Logo placeholder</div>
 			<Navigation />
 		</header>
 	);
